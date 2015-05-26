e5d3af9f8c2989061c4c0141307cdb54926e8644
l30n13 <mrtonmoy12@gmail.com>
2015-05-26 17:35:49 +0600

    Class11 added

--- Class11/PersonalDetails/app/src/main/java/com/android/tonmoy/personaldetails/MainActivity.java
+++ Class11/PersonalDetails/app/src/main/java/com/android/tonmoy/personaldetails/MainActivity.java
@@ -1,18 +1,87 @@
 package com.android.tonmoy.personaldetails;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 
+@SuppressWarnings("deprecation")
 public class MainActivity extends ActionBarActivity {
+
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView;
+    private int year, month, day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dateView = (TextView) findViewById(R.id.birthday);
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month + 1, day);
+
     }
+
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "ca", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker arg0, int year, int month, int day) {
+            showDate(year, month + 1, day);
+        }
+    };
+
+    private void showDate(int year, int month, int day) {
+        dateView.setText(new StringBuilder().append(day).append("/").append(month).append("/").append(year));
+    }
+
+    public void saveAll(View view) {
+        EditText name = (EditText) findViewById(R.id.name);
+        TextView birthday = (TextView) findViewById(R.id.birthday);
+        RadioGroup gender = (RadioGroup) findViewById(R.id.gender);
+        int selectGender = gender.getCheckedRadioButtonId();
+        String g = "";
+        switch (selectGender) {
+            case 0:
+                Toast.makeText(this, "Male", Toast.LENGTH_SHORT).show();
+                break;
+            case 1:
+                Toast.makeText(this, "Female", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        Toast.makeText(this, "This is test " + selectGender, Toast.LENGTH_SHORT).show();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
